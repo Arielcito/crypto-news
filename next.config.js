35d493c52fad8d7313d573416f7d108b493d19ed
Arielcito <serato.arieli@gmail.com>
2025-04-11 22:30:45 -0300

    feat: update next.config.js and remove next.config.ts
    - Merged configurations from next.config.ts into next.config.js for better organization.
    - Added a new remote image pattern for WordPress hostname.
    - Implemented a redirect from "/admin" to the WordPress admin URL.
    - Set metadata base URL using NEXT_PUBLIC_SITE_URL or a default value.

--- next.config.js
+++ next.config.js
@@ -6,8 +6,24 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'images.unsplash.com',
       },
+      {
+        protocol: 'https',
+        hostname: process.env.WORDPRESS_HOSTNAME,
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: "/admin",
+        destination: `${process.env.WORDPRESS_URL}/wp-admin`,
+        permanent: true,
+      },
+    ];
+  },
+  metadataBase: new URL(process.env.NEXT_PUBLIC_SITE_URL || 'https://crypto-news-7iqshbwpy-arielcitos-projects.vercel.app'),
 };
 
 module.exports = nextConfig; 
